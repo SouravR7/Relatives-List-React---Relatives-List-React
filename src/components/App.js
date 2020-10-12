e26b4976fa08e5437c17e6c774a698a4dea5cf4b
Sourav Routh <67389838+SouravR7@users.noreply.github.com>
2020-10-12 13:03:34 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+
+class App extends Component {
+    render() {
+
+        return(
+           <>
+//write your code here
+	</>
+        )
+    }
+}
+
+
+export default App;
