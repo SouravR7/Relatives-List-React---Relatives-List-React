c3878c5bac5b01cbf4f82f718405294a9dce1b48
Sourav Routh <67389838+SouravR7@users.noreply.github.com>
2020-10-30 22:54:31 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-           <>
-//write your code here
-	</>
-        )
-    }
+  render() {
+    let relatives = ["Souvik", "Nilanjan", "Anirban", "Dipanjan"];
+    return (
+      <>
+        <ol key="relativeList">
+          {relatives.map((relative, index) => (
+            <li key={`relativeListitem ${index + 1}`}>{relative}</li>
+          ))}
+        </ol>
+      </>
+    );
+  }
 }
-
 
 export default App;
